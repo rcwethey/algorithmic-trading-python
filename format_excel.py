8881dcccb71ccfeed08f01dc3dddb50642275b54
Your Name <you@example.com>
2021-07-24 23:17:17 -0400

    Giant Push to Github

--- format_excel.py
+++ format_excel.py
@@ -12,21 +12,45 @@ def basic_format_update(key, value):
     new_format[f'{key}'] = f'{value}'
 
 
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+
+
 def format_excelsheet(writer, columns):
-    # Create the formats you need for our Xlsx file
     string_format = writer.book.add_format(basic_format)
     dollar_format = writer.book.add_format(
         basic_format_update('num_format', '$0.00'))
     integer_format = writer.book.add_format(
         basic_format_update('num_format', '0'))
+    percent_format = writer.book.add_format(
+        basic_format_update('num_format', '0.0%'))
 
-    # create a dictorionary of values to help loop over
+    formats = {'string': string_format, 'dollar': dollar_format,
+               'int': integer_format, 'percent': percent_format}
     columns_formats = {
-        'A': [columns[0], string_format],
-        'B': [columns[1], dollar_format],
-        'C': [columns[2], dollar_format],
-        'D': [columns[3], integer_format]
+        # 'A': [columns[0], string_format],
+        # 'B': [columns[1], dollar_format],
+        # 'C': [columns[2], dollar_format],
+        # 'D': [columns[3], integer_format]
     }
+
+    for index, column in enumerate(columns):
+        if "Ticker" in column:
+            append_to_columns_formats(
+                index, column, columns_formats, formats=formats.get('string'))
+        if "Stock" in column:
+            print('Yeup')
+        if "Market" in column:
+            print('Yeup')
+        if "Buy" in column:
+            print('Yeup')
+        if "HQM Score" in column:
+            print('Yeup')
+        if "Price Return" in column:
+            print('Yeup')
+        if "Return Percentile" in column:
+            print('Yeup')
+
+    print(columns_formats)
 
     # Format
     for column in columns_formats.keys():
@@ -34,3 +58,7 @@ def format_excelsheet(writer, columns):
             f'{column}1', columns_formats[column][0], columns_formats[column][1])
         writer.sheets[SHEET_NAME].set_column(
             f'{column}:{column}', 20, columns_formats[column][1])
+
+
+def append_to_columns_formats(index, column, columns_formats, formats):
+    return columns_formats[f'{alphabet[index]}'].append(column[index]).extend(formats)
--- quantitative_momentum_strategy.py
+++ quantitative_momentum_strategy.py
@@ -1,7 +1,8 @@
 from secrets import IEX_CLOUD_API_TOKEN
 from helper_functions import get_portfolio_size, get_chunks, position_size, read_stocks_file
 import numpy as np
-from scipy import stats
+from scipy.stats import percentileofscore as score
+from statistics import mean
 import pandas as pd
 import requests
 import xlsxwriter
@@ -10,10 +11,10 @@ portfolio_size = get_portfolio_size()
 stocks = read_stocks_file()
 symbol_strings = get_chunks(stocks, 100)
 
-columns = ['Ticker', 'Stock Price',
-           '1 Year Price Return', 'Number of Shares to Buy']
+hqm_columns = ['Ticker', 'Stock Price', 'Number of Shares to Buy', 'HQM Score', 'One-Year Price Return', 'One-Year Return Percentile', 'Six-Month Price Return',
+               'Six-Month Return Percentile', 'Three-Month Price Return', 'Three-Month Return Percentile', 'One-Month Price Return', 'One-Month Return Percentile', ]
 
-dataframe = pd.DataFrame(columns=columns)
+dataframe = pd.DataFrame(columns=hqm_columns)
 
 for symbol_string in symbol_strings:
     batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=price,stats&token={IEX_CLOUD_API_TOKEN}'
@@ -21,11 +22,45 @@ for symbol_string in symbol_strings:
     for symbol in symbol_string.split(','):
         dataframe = dataframe.append(
             pd.Series(
-                [symbol, data[symbol]['price'], data[symbol]['stats']['year1ChangePercent'], 'N/A'], index=columns), ignore_index=True)
+                [
+                    symbol,
+                    data[symbol]['price'],
+                    'N/A',
+                    'N/A',
+                    data[symbol]['stats']['year1ChangePercent'],
+                    'N/A',
+                    data[symbol]['stats']['month6ChangePercent'],
+                    'N/A',
+                    data[symbol]['stats']['month3ChangePercent'],
+                    'N/A',
+                    data[symbol]['stats']['month1ChangePercent'],
+                    'N/A',
+                ],
+                index=hqm_columns),
+            ignore_index=True)
 
-dataframe.sort_values('1 Year Price Return', ascending=False, inplace=True)
+time_periods = ['One-Year', 'Six-Month', 'Three-Month', 'One-Month']
+
+for row in dataframe.index:
+    for time_period in time_periods:
+        change_col = f'{time_period} Price Return'
+        percentile_col = f'{time_period} Return Percentile'
+        dataframe.loc[row, percentile_col] = score(
+            dataframe[change_col], dataframe.loc[row, change_col])
+
+for row in dataframe.index:
+    momentum_percetiles = []
+    for time_period in time_periods:
+        momentum_percetiles.append(dataframe.loc[
+            row, f'{time_period} Return Percentile'])
+    dataframe.loc[row, 'HQM Score'] = mean(momentum_percetiles)
+
+dataframe.sort_values('HQM Score', ascending=False, inplace=True)
 dataframe = dataframe[:50]
-dataframe.reset_index(inplace=True)
+dataframe.reset_index(inplace=True, drop=True)
 
 position_size(portfolio_size, dataframe)
+writer = pd.ExcelWriter('momentum_strategy.xlsx', engine='xlsxwriter')
+dataframe.to_excel(writer, sheet_name = "Momentum Stategy", index=False)
+
 print(dataframe)
